9e3caa87dc3d71b25a315020dc38c683d6b0655d
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-06-14 20:08:15 +0530

    Datatypes and operators in js

---
+++ javascript/02_datatypes_and_operators.js
@@ -0,0 +1,40 @@
+// number
+let num = 3;
+console.log(typeof(num));
+console.log(3 + 10.0);
+console.log(3 + 10.21);
+
+// string
+let name = "abc";
+let name2 = "abc";
+console.log(name + name2);
+
+// booleans
+let isloggedin = true;
+console.log(Number(isloggedin));
+console.log(isloggedin + 10);
+
+// null - space is used, the var is stored somewhere with its val null
+let date = null;
+console.log(date);
+
+// undefined - no space used
+let c = undefined;
+
+// objects
+const person = {
+    firstName : "devanshi",
+    lastName : "bhatt",
+    age : 19,
+}
+
+console.log(person);
+
+// string
+console.log('1' + 2);
+
+// number
+console.log('12' / 10);
+
+// NaN - not a number
+console.log('a' * 1);
